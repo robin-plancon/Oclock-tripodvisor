bba4f706ad9aa1f0f6bf8ef15c295aab878df99c
robin-plancon <robin.plancon@outlook.com>
2023-05-27 12:10:10 +0200

    add theme managing

--- js/app.js
+++ js/app.js
@@ -6,65 +6,169 @@
     5. Ajouter un eventlistener qui verifie la distance scroller 
     pour afficher le popup newsletter
 
-    utiliser le parametre hidden pour cacher la newsletter
+    bonus
+    Pour gerer les themes: ajouter la class sur le body
 */
 
 const app = {
-
-    NewsletterLinkElement: document.getElementById("newsletter-link"),
-    NewsletterPopUpElement: document.querySelector(".newsletter"),
+    newsletterLinkElement: document.getElementById("newsletter-link"),
+    newsletterPopUpElement: document.querySelector(".newsletter"),
+    newsletterSubmitButtonElement: document.getElementById(
+        "submit-subcriber-email"
+    ),
+    switchThemeButtonElement: document.getElementById("theme-switch"),
+    switchColorsElement: document.getElementById("theme-colors"),
     lastScrollPosition: 0,
     isNewsletterHidden: true,
+    isDarkModeTheme: false,
+    currentColorTheme: "",
+    forbiddenDomains: [
+        "@yopmail.com",
+        "@yopmail.fr",
+        "@yopmail.net",
+        "@cool.fr.nf",
+        "@jetable.fr.nf",
+        "@courriel.fr.nf",
+        "@moncourrier.fr.nf",
+        "@monemail.fr.nf",
+        "@monmail.fr.nf",
+        "@hide.biz.st",
+        "@mymail.infos.st",
+    ],
 
-    setElementListenable (element, eventListenerType) {
-        element.addEventListener(eventListenerType, app.handleElementClick);
+    setElementListenable(element, eventListenerType, eventHandler) {
+        element.addEventListener(eventListenerType, eventHandler);
     },
 
-    handleElementClick (Event) {
+    setWindowsScrollListenable() {
+        document.addEventListener("scroll", app.handleWindowsScroll);
+    },
+
+    // Newsletter part
+
+    handleNewsletterLinkClick(Event) {
         Event.preventDefault();
-        const targetLink = Event.target;
-        if(targetLink.id === "newsletter-link") {
-            app.setNewsletterPopUpOpen(app.NewsletterPopUpElement);
-            return;
-        }
-        if(targetLink.classList.contains("newsletter__close")) {
-            app.setNewsletterPopUpClose(app.NewsletterPopUpElement);
-            return;
+        app.setNewsletterPopUpOpen(app.newsletterPopUpElement);
+    },
+
+    handleNewsletterClosingClick(Event) {
+        Event.preventDefault();
+        app.setNewsletterPopUpClose(app.newsletterPopUpElement);
+    },
+
+    handleNewsletterSubcriberSubmit(Event) {
+        Event.preventDefault();
+        const email = document.getElementById("subscriber-email").value;
+        if (app.validateEmail(email)) {
+            alert("Merci de vous être inscrit à notre newsletter.");
         }
     },
 
-    setWindowsScrollListenable () {
-        document.addEventListener("scroll", app.handleWindowsScroll); 
-    },
-
-    handleWindowsScroll () {
-        if (window.scrollY - app.lastScrollPosition >= 300 && app.isNewsletterHidden) {
-            app.setNewsletterPopUpOpen(app.NewsletterPopUpElement);
+    handleWindowsScroll() {
+        if (
+            window.scrollY - app.lastScrollPosition >= 300 &&
+            app.isNewsletterHidden
+        ) {
+            app.setNewsletterPopUpOpen(app.newsletterPopUpElement);
             app.lastScrollPosition = window.scrollY;
             return;
         }
         if (!app.isNewsletterHidden) {
             app.lastScrollPosition = window.scrollY;
         }
+        if (app.isNewsletterHidden && window.scrollY < app.lastScrollPosition) {
+            app.lastScrollPosition = window.scrollY;
+        }
     },
 
-    setNewsletterPopUpOpen (element) {
+    setNewsletterPopUpOpen(element) {
         element.classList.remove("newsletter--hidden");
         app.isNewsletterHidden = false;
     },
 
-    setNewsletterPopUpClose (element) {
+    setNewsletterPopUpClose(element) {
         element.classList.add("newsletter--hidden");
         app.isNewsletterHidden = true;
     },
 
-    init () {
-        app.setElementListenable(app.NewsletterLinkElement, "click");
-        app.setElementListenable(app.NewsletterPopUpElement.querySelector(".newsletter__close"), "click");
-        app.setWindowsScrollListenable();
-        app.setNewsletterPopUpClose(app.NewsletterPopUpElement);
+    validateEmail(email) {
+        const emailRegex = new RegExp(
+            /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/,
+            "gm"
+        );
+        if (!emailRegex.test(email)) {
+            alert("adresse email non valide.");
+            return false;
+        }
+        app.forbiddenDomains.map((forbiddenDomain) => {
+            if (email.includes(forbiddenDomain)) {
+                alert(
+                    `le domaine ${forbiddenDomain} de l'adresse mail n'est pas authorise`
+                );
+                return false;
+            }
+        });
+        return true;
     },
 
-}
+    // Theme part
+
+    handleChangeThemeClick() {
+        app.isDarkModeTheme = !app.isDarkModeTheme;
+        const bodyElement = document.getElementsByTagName("body")[0];
+        if (app.isDarkModeTheme) {
+            bodyElement.classList.add("theme-dark");
+            return;
+        } else {
+            bodyElement.classList.remove("theme-dark");
+            return;
+        }
+    },
+
+    handleChangeThemeColorClick(Event) {
+        const target = Event.target;
+        const bodyElement = document.getElementsByTagName("body")[0];
+        if(app.currentColorTheme === ""){
+            bodyElement.classList.add(target.id);
+            app.currentColorTheme = target.id;
+            return;
+        }
+        bodyElement.classList.replace(app.currentColorTheme, target.id);
+        app.currentColorTheme = target.id;
+    },
+
+    initializeColorThemeButtons() {
+        colorButtons = app.switchColorsElement.children;
+        for(colorButton of colorButtons) {
+            app.setElementListenable(colorButton, "click", this.handleChangeThemeColorClick);
+        }
+    },
+
+    init() {
+        app.setElementListenable(
+            app.newsletterLinkElement,
+            "click",
+            app.handleNewsletterLinkClick
+        );
+        app.setElementListenable(
+            app.newsletterPopUpElement.querySelector(".newsletter__close"),
+            "click",
+            app.handleNewsletterClosingClick
+        );
+        app.setElementListenable(
+            app.newsletterSubmitButtonElement,
+            "click",
+            app.handleNewsletterSubcriberSubmit
+        );
+        app.setWindowsScrollListenable();
+        app.setNewsletterPopUpClose(app.newsletterPopUpElement);
+        app.setElementListenable(
+            app.switchThemeButtonElement,
+            "click",
+            app.handleChangeThemeClick
+        );
+        app.initializeColorThemeButtons();
+    },
+};
 
 document.addEventListener("DOMContentLoaded", app.init);
